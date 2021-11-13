1314e09a9f57c24f0cfec3428ef297f5bd4f8fd3
Concept <conceptcodes@gmail.com>
2021-11-13 13:24:47 -0500

    new font added and buttons look crispy

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # Landing - React.js + Tailwindcss
-This is a [React.js](https://reactjs.org) web application powered by [Next.js](https://nextjs.org) & [Tailwindcss](https://tailwindcss.com). The concept for the UI came from a post I saw on pinterest by [Dstudio Agency](https://www.instagram.com/dstudioagency/). I liked the minimalistic layout and decided to build it :)
+This is a single page web application built in [React.js](https://reactjs.org) and powered by [Next.js](https://nextjs.org) & [Tailwindcss](https://tailwindcss.com). The concept for the UI came from a post I saw on pinterest by [Dstudio Agency](https://www.instagram.com/dstudioagency/). I liked the minimalistic layout and decided to build it :)
 
 > [Live Demo](https://conceptcodes.dev)
 
-![demo_screenshot](./demo.png)
+![demo_screenshot](./dstudio_landing.png)
 
 ## Requirement
 - Next.js
--- pages/index.js
+++ pages/index.js
@@ -23,54 +23,54 @@ let navItems = ['products','our work','services']
 
 export default function Home() {
   return (
-    <div className="bg-black flex flex-col justify-around min-h-screen">
+    <div className="dark:bg-black flex flex-col justify-around h-screen">
       <Head>
         <title>Landing</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <header class="flex justify-around p-5">
-      <div className="text-white">hello@cargo.io</div>
-      <div className="flex text-gray-400 capitalize">
-        { navItems.map((x, index) => <div key={index} className="mx-3 cursor-pointer hover:text-white">{x}</div> )}
+      <header class="flex justify-around">
+      <div className="dark:text-white">hello@cargo.io</div>
+      <div className="flex dark:text-gray-400 capitalize">
+        { navItems.map((x, index) => <div key={index} className="mx-3 cursor-pointer hover:text-gray-800 dark:hover:text-white">{x}</div> )}
       </div>
       <div>
-        <a href="" className="text-gray-500 mx-3 uppercase hover:text-white">
+        <a href="" className="dark:text-gray-500 mx-3 uppercase hover:text-white">
           faq
         </a>
-        <button className="bg-blue-600 text-white capitalize h-8 rounded-xl">
+        <button className="bg-blue-600 text-white capitalize px-4  h-8 rounded-2xl">
           get started
         </button>
       </div>
     </header>
-      <div className="pt-3 sm:mt-50 flex flex-col mx-10  py-2">
+      <div className="sm:mt-50 flex flex-col mx-10">
         <div className="flex flex-col items-center">
-          <h1 className="text-white text-6xl">
-            Cargo <span className="text-gray-500 mx-2">Design</span> Agency
+          <h1 className="dark:text-white text-black text-6xl">
+            Cargo <span className="dark:text-gray-500 text-blue-600 mx-1">Design</span> Agency
           </h1>
           <h4 className="text-gray-600 text-xl p-3">
             Ahead with 100+ amazing clients with 20+ Country Served the best
             design materials
           </h4>
-          <button className="capitalize text-white bg-blue-600 rounded-xl">
+          <button className="capitalize px-6 py-2 text-white bg-blue-600 rounded-3xl">
             get started
           </button>
         </div>
         <div className="">
           <div className="flex justify-between pb-10">
-            <div className="capitalize text-xl text-white">case studies</div>
-            <div className="capitalize text-white underline">view all</div>
+            <div className="capitalize text-xl dark:text-white">case studies</div>
+            <div className="capitalize dark:text-white underline">view all</div>
           </div>
           <div className="flex justify-between">
             {caseStudies.map((study, index) => 
-              (<div key={index} className="flex group flex-grow mx-2 flex-col p-3 h-45 rounded-lg hover:bg-blue-600 border-4 border-gray-500 border-opacity-25">
+              (<div key={index} className="flex group flex-grow mx-2 flex-col p-3 h-45 rounded-lg hover:bg-blue-600 border-4 border-black dark:border-gray-500 border-opacity-25 hover:border-opacity-0">
                 <div className="m-3 group-hover:text-white">
-                  <div className="flex text-gray-600 justify-between pb-5">
+                  <div className="flex dark:text-gray-600 justify-between pb-5">
                     <div className="uppercase">{study.category}</div>
                     <div className="capitalize">{study.application_type}</div>
                   </div>
-                  <div className="text-3xl capitalize text-white">{study.title}</div>
-                  <p className="text-gray-600 truncate pb-5">At vero eos et accusamus et o</p>
-                  <div className="text-white capitalize">view case study</div>
+                  <div className="text-3xl capitalize dark:text-white">{study.title}</div>
+                  <p className="dark:text-gray-600 truncate pb-5">At vero eos et accusamus et o</p>
+                  <div className="dark:text-white capitalize">view case study</div>
                   <span>
                     {/* <ArrowNarrowRight /> */}
                   </span>
